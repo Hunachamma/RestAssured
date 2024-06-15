b57415d13afb1bf9c18397661eb3d6be08bceb8e
Hunachamma <leenahampani272@gmail.com>
2024-06-16 00:19:07 +0530

    My First Commit

---
+++ src/test/java/api/endpoints/UserEndpoints.java
@@ -0,0 +1,64 @@
+package api.endpoints;
+import static io.restassured.RestAssured.given;
+
+import api.payload.User;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+
+
+
+
+
+public class UserEndpoints {
+	public static Response createUser(User payload){
+		
+		Response response=given().
+				contentType(ContentType.JSON).
+				accept(ContentType.JSON).
+				body(payload)
+				.when().post(Routes.user_post_url);
+		
+		return response;
+	
+		
+		
+	}
+public static Response readUser(String username){
+		
+		Response response=given().pathParam("username",username)
+				
+				.when().get(Routes.user_get_url);
+		
+		return response;
+	
+		
+		
+	}
+public static Response updateUser(User payload, String username){
+	
+	Response response=given().
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(payload)
+			.pathParam("username",username)
+			.when().post(Routes.user_put_url);
+	
+	return response;
+
+	
+	
+}
+public static Response deleteUser(String username){
+	
+	Response response=given().pathParam("username",username)
+			
+			.when().get(Routes.user_delete_url);
+	
+	return response;
+
+	
+	
+}
+
+}
